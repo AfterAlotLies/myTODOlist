52b9886a248dabb4b68475e24a91d7976643810b
Vyacheslav <guzxaroh@gmail.com>
2023-11-27 20:16:24 +0700

    localization enums

---
+++ myTodolist/Enums/LocalizatedSegmentControlCases.swift
@@ -0,0 +1,14 @@
+//
+//  LocalizatedSegmentControlCases.swift
+//  myTodolist
+//
+//  Created by Vyacheslav on 23.11.2023.
+//
+
+import Foundation
+
+enum LocalizatedSegmentControlCases: String {
+    case notImmediatly = "Не очень срочно"
+    case inNextDays = "В ближ. дни"
+    case immediatly = "Срочно"
+}
---
+++ myTodolist/Enums/SegmentControlCases.swift
@@ -0,0 +1,14 @@
+//
+//  SegmentControlCases.swift
+//  myTodolist
+//
+//  Created by Vyacheslav on 23.11.2023.
+//
+
+import Foundation
+
+enum SegmentControlCases: Int {
+    case notImmediatly
+    case inNextDays
+    case immediatly
+}
