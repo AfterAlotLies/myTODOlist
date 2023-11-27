097f4363f169f93a039324edcb4bb7c2521d40c1
Vyacheslav <guzxaroh@gmail.com>
2023-11-27 20:14:37 +0700

    prototype of TableView

---
+++ myTodolist/TableViewCells/MainTableViewCells.swift
@@ -0,0 +1,14 @@
+//
+//  MainTableViewCells.swift
+//  myTodolist
+//
+//  Created by Vyacheslav on 26.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class MainTableViewCells: UITableViewCell {
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+}
---
+++ myTodolist/TableViewCells/UserInfoTableViewCells.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfoTableViewCells.swift
+//  myTodolist
+//
+//  Created by Vyacheslav on 26.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class UserInfoTableViewCells: UITableViewCell {
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
